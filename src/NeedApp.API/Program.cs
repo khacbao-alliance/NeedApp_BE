9b0d9c5f0ff2b2bc4332bd74bbc305861292995c
caokhacbao <bao.caokhac@allianceitsc.com>
2026-04-07 13:54:25 +0700

    fix: fix cors

--- src/NeedApp.API/Program.cs
+++ src/NeedApp.API/Program.cs
@@ -72,7 +72,8 @@ builder.Services.AddCors(options =>
         policy.WithOrigins(
                 "http://localhost:3000",
                 "http://localhost:5173",
-                "https://localhost:3000"
+                "https://localhost:3000",
+                "https://needanapp.netlify.app"
             )
             .AllowAnyMethod()
             .AllowAnyHeader()
